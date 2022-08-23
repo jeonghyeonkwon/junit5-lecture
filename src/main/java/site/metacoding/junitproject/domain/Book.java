6a47745866df35f36efc4399c15eda67cd774794
jeonghyeonkwon <givejeong2468@gmail.com>
2022-08-23 10:04:49 +0900

    Book 엔티티 생성 완료

--- src/main/java/site/metacoding/junitproject/domain/Book.java
+++ src/main/java/site/metacoding/junitproject/domain/Book.java
@@ -1,4 +1,30 @@
 package site.metacoding.junitproject.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Book {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20,nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
