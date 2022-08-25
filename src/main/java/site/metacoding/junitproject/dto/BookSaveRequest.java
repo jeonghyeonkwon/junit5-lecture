636e37d53ad3aae1273bc103fff84191c2b2e68d
jeonghyeonkwon <givejeong2468@gmail.com>
2022-08-25 17:28:37 +0900

    서비스 레이터 테스트 - 실제 DB로

--- src/main/java/site/metacoding/junitproject/dto/BookSaveRequest.java
+++ src/main/java/site/metacoding/junitproject/dto/BookSaveRequest.java
@@ -1,6 +1,7 @@
 package site.metacoding.junitproject.dto;
 
 
+import lombok.Getter;
 import lombok.Setter;
 import site.metacoding.junitproject.domain.Book;
 
@@ -8,6 +9,7 @@ import site.metacoding.junitproject.domain.Book;
 * Controller에서 Setter가 호출되면서 Dto에 값이 채워짐
 * */
 @Setter
+@Getter
 public class BookSaveRequest {
     private String title;
     private String author;
--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,7 +1,38 @@
 package site.metacoding.junitproject.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import site.metacoding.junitproject.dto.BookResponse;
+import site.metacoding.junitproject.dto.BookSaveRequest;
+import site.metacoding.junitproject.repository.BookRepository;
+import site.metacoding.junitproject.util.MailSenderStub;
 
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
 class BookServiceTest {
+
+    //이렇게 주입하면 서비스만 테스트하고 싶은데 repository까지 테스트 해야 된다. 그래서 mockito같은 걸 쓴다
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void 책등록하기_테스트(){
+        //given
+
+
+        BookSaveRequest dto = new BookSaveRequest();
+        dto.setTitle("junit5");
+        dto.setAuthor("실습자");
+
+        //가짜 객체
+        MailSenderStub mailSenderStub = new MailSenderStub();
+        BookService bookService = new BookService(bookRepository,mailSenderStub);
+        BookResponse saved = bookService.책등록하기(dto);
+
+        assertEquals(dto.getTitle(),saved.getTitle());
+        assertEquals(dto.getAuthor(),saved.getAuthor());
+
+    }
 
 }
